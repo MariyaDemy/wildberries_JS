0360f8e39bf91afca50c0f50d4853a6db48d06d7
MariyaDemy <Mariya.Demi373@yandex.ru>
2022-02-26 20:24:05 +0300

    add goods rendering, search

--- js/getGoods.js
+++ js/getGoods.js
@@ -2,31 +2,76 @@ const getGoods = () => {
 
     const links = document.querySelectorAll('.navigation-link');
 
-    const getData = () => {
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list');
+
+        goodsContainer.innerHTML = '';
+
+        goods.forEach((good) => {
+            const goodBlock = document.createElement('div');
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-6');
+            goodBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${good.label ? 'null' : 'd-none'}">${good.label}</span>
+                    <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">${'$' + good.price}</span>
+                    </button>
+                </div>
+            `
+            goodsContainer.append(goodBlock)
+
+        })
+
+    }
+
+    const getData = (value, category) => {
         fetch('https://wildberriesjs-b8f72-default-rtdb.firebaseio.com/db.json')
             .then((response) => response.json())
             .then((data) => {
-                localStorage.setItem('data', JSON.stringify(data));
+
+                const array = category ? data.filter((item) => item[category] === value) : data
+                // значение в данных с сервера равны тексту ссылок
+                // server: gender: Womens + в ссылке текст Womens               
+
+                localStorage.setItem('goods', JSON.stringify(array));
+
+                if (window.location.pathname !== "/goods.html") {
+                    window.location.href = '/goods.html'
+                } else {
+                    renderGoods(array);
+                }
             })
     }
 
     links.forEach((link) => {
         link.addEventListener('click', (event) => {
             event.preventDefault();
-            getData();
+
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
+
+            getData(linkValue, category);
         })
     })
 
+    if (localStorage.getItem('goods') && window.location.pathname === "/goods.html") {
+        renderGoods(JSON.parse(localStorage.getItem('goods')))
+    }
 
-    localStorage.setItem('goods', JSON.stringify({
-        name: 'all'
-    }))
 
-    const goods = JSON.parse(localStorage.getItem('goods'));
-    console.log(goods)
+    // localStorage.setItem('goods', JSON.stringify({
+    //     name: 'all'
+    // }))
 
-    localStorage.removeItem('goods');
-    console.log(localStorage);
+    // const goods = JSON.parse(localStorage.getItem('goods'));
+    // console.log(goods)
+
+    // localStorage.removeItem('goods');
+    // console.log(localStorage);
 
 }
 getGoods();
